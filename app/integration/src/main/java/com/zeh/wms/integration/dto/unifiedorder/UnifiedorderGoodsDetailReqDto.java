ba59e5b3e9bd935be41f9d1e8adf34f5f428f909
huzhongyuan <huzhongyuan@gmail.com>
2018-02-07 16:27:28 +0800

    wechat客户端

---
+++ app/integration/src/main/java/com/zeh/wms/integration/dto/unifiedorder/UnifiedorderGoodsDetailReqDto.java
@@ -0,0 +1,33 @@
+package com.zeh.wms.integration.dto.unifiedorder;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author hzy24985
+ * @version $Id: UnifiedorderGoodsDetailReqDto, v 0.1 2018/2/7 15:58 hzy24985 Exp $
+ */
+@Data
+public class UnifiedorderGoodsDetailReqDto implements Serializable {
+
+    private static final long serialVersionUID = -6867155085809507941L;
+
+    /**商品编码*/
+    @JSONField(name = "goods_id")
+    private String goodsId;
+
+    @JSONField(name = "wxpay_goods_id")
+    private String wxpayGoodsId;
+
+    @JSONField(name = "goods_name")
+    private String goodsName;
+
+    /** 用户购买的数量 */
+    private int quantity;
+
+    /** 单位为：分。如果商户有优惠，需传输商户优惠后的单价(例如：用户对一笔100元的订单使用了商场发的纸质优惠券100-50，则活动商品的单价应为原单价-50) */
+    private int price;
+
+}
---
+++ app/integration/src/main/java/com/zeh/wms/integration/dto/unifiedorder/UnifiedorderRespDto.java
@@ -0,0 +1,72 @@
+package com.zeh.wms.integration.dto.unifiedorder;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 商户在小程序中先调用该接口在微信支付服务后台生成预支付交易单，返回正确的预支付交易后调起支付。
+ * 返回参数。
+ * @author hzy24985
+ * @version $Id: UnifiedorderRespDto, v 0.1 2018/2/7 15:20 hzy24985 Exp $
+ */
+@Data
+public class UnifiedorderRespDto implements Serializable {
+    private static final long serialVersionUID = 1406280387114609736L;
+
+    /**
+     * SUCCESS/FAIL
+     */
+    @XStreamAlias("return_code")
+    private String            returnCode;
+
+    /** 返回信息，如非空，为错误原因 签名失败 */
+    @XStreamAlias("return_msg")
+    private String            returnMsg;
+
+    /** ===========以下字段在return_code为SUCCESS的时候有返回==================*/
+    @XStreamAlias("appid")
+    private String            appId;
+
+    /** 商户号 */
+    @XStreamAlias("mch_id")
+    private String            merchantId;
+
+    /** 设备号 */
+    @XStreamAlias("device_info")
+    private String            deviceInfo;
+
+    /** 随机字符串 */
+    @XStreamAlias("nonce_str")
+    private String            nonceStr;
+
+    /** 签名 */
+    private String            sign;
+
+    /** 业务结果 SUCCESS/FAIL */
+    @XStreamAlias("result_code")
+    private String            resultCode;
+
+    /** 错误代码 UnifiedorderErrorCodeEnum */
+    @XStreamAlias("err_code")
+    private String            errCode;
+
+    /** 错误代码描述 */
+    @XStreamAlias("err_code_des")
+    private String            errCodeDes;
+
+    /** ==========以下字段在return_code 和result_code都为SUCCESS的时候有返回======== */
+
+    /** 交易类型 小程序取值如下：JSAPI*/
+    @XStreamAlias("trade_type")
+    private String            tradeType;
+
+    /** 预支付交易会话标识: 微信生成的预支付会话标识，用于后续接口调用中使用，该值有效期为2小时*/
+    @XStreamAlias("prepay_id")
+    private String            prepayId;
+
+    /**二维码链接: trade_type为NATIVE时有返回，用于生成二维码，展示给用户进行扫码支付 */
+    @XStreamAlias("code_url")
+    private String            codeUrl;
+}
