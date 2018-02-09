9f5d9ef197ce26cdf1bfd53dbc7e02859e6c5f56
hzy24985 <hzy24985@ly.com>
2018-02-09 12:57:52 +0800

    excel导入例子

--- app/web/src/main/java/com/zeh/wms/web/controller/order/ExpressOrderController.java
+++ app/web/src/main/java/com/zeh/wms/web/controller/order/ExpressOrderController.java
@@ -1,5 +1,7 @@
 package com.zeh.wms.web.controller.order;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.zeh.jungle.dal.paginator.PageList;
 import com.zeh.wms.biz.exception.ServiceException;
 import com.zeh.wms.biz.model.ExpressOrderVO;
@@ -8,14 +10,26 @@ import com.zeh.wms.dal.operation.expressorder.FindPageQuery;
 import com.zeh.wms.dal.operation.expressorder.GetAllByParsQuery;
 import com.zeh.wms.web.constant.ExcelConstant;
 import com.zeh.wms.web.controller.BaseController;
+import com.zeh.wms.web.form.AgentImportModel;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.jxls.reader.ReaderBuilder;
+import org.jxls.reader.ReaderConfig;
+import org.jxls.reader.XLSReader;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.xml.sax.SAXException;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
 
 /**
  * The type Express order controller.
@@ -64,5 +78,38 @@ public class ExpressOrderController extends BaseController {
     @ResponseBody
     public ResponseEntity<byte[]> export(GetAllByParsQuery query, HttpServletRequest request) throws ServiceException {
         return expressOrderService.export(query, getRealFileName(request, ExcelConstant.SF_FILE_PATH));
+    }
+
+    @RequestMapping(value = "import", method = RequestMethod.GET)
+    @ResponseBody
+    public String importData(HttpServletRequest request) throws ServiceException {
+        String configPath = getRealFileName(request, "/import/ai_matedata.xml");
+        String exampleData = getRealFileName(request, "/export/ai_template.xlsx");
+
+        try(InputStream xmlInputStream = new FileInputStream(configPath)) {
+            ReaderConfig.getInstance().setSkipErrors(true);
+            XLSReader reader = ReaderBuilder.buildFromXML(xmlInputStream);
+            try (InputStream xlsInputStream = new FileInputStream(exampleData)) {
+                List<AgentImportModel> modelList = Lists.newArrayList();
+                Map<String, Object> beans = Maps.newHashMap();
+                beans.put("list", modelList);
+
+                logger.info("Reading the data...");
+                reader.read(xlsInputStream, beans);
+                logger.info("Read " + modelList.size() + " model into `list` list");
+                for (AgentImportModel model : modelList) {
+                    logger.info(model.toString());
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (InvalidFormatException e) {
+            e.printStackTrace();
+        }
+        return "OK";
     }
 }
