92df159daab086a3ebf56b0257e57f59a89ad546
mygit <mygit@115.29.196.239>
2018-02-13 17:24:20 +0800

    添加绑定商品界面

--- webdocs/static/js/wms/qr/code/view.js
+++ webdocs/static/js/wms/qr/code/view.js
@@ -5,10 +5,9 @@
 $(document).ready(function () {
     debugger;
     new Vue({
-        el: '#qrcode_bind',
+        el: '#modalForm',
         data: {
-            model: {},
-            agent: express_vm.agents
+            model: {}
         },
         ready: function () {
             this.init();
@@ -16,12 +15,62 @@ $(document).ready(function () {
         methods: {
             init: function () {
                 var self = this;
+
+                /**
                 $.ajax({
                     type: 'GET',
                     url: __ctx + "/qr/code/one",
-                    data: {id: $("#objId").val()}
+                    data: {id: formData.id}
                 }).done(function (result) {
                     self.model = result;
+                });*/
+            },
+
+            // 提交数据
+            submitData: function () {
+                $("#modalForm").data('bootstrapValidator', null);
+                this.validator();
+                var me = this.model;
+                var url = __ctx + "/qr/code/doBind?id=" + me.id + "&commondityId=" + me.commodityId;
+                var method = "POST";
+                $("#modalForm").data('bootstrapValidator').validate();
+                if ($("#modalForm").data('bootstrapValidator').isValid()) {
+                    $.ajax({
+                        type: method,
+                        url: url,
+                        // data: JSON.stringify(me),
+                        dataType: "json",
+                        contentType: "application/json",
+                        //成功返回之后调用的函数
+                        success: function (data) {
+                            if (data && data.success) {
+                                $('#formModal').modal('hide');
+                                toastr.success('操作成功', {timeOut: 1500, positionClass: "toast-top-center"});
+                                qrCode_vm.preQuery();
+                            } else {
+                                toastr.error(data.errorMessage, {timeOut: 1500, positionClass: "toast-top-center"});
+                            }
+                        },
+                        error: function (err) {
+                            toastr.error(err, {timeOut: 1500, positionClass: "toast-top-center"});
+                        }
+                    });
+                }
+            },
+
+            // 校验数据
+            validator: function () {
+                $('#modalForm').bootstrapValidator ({
+                    message: 'This value is not valid',
+                    fields: {
+                        commodityId: {
+                            validators: {
+                                notEmpty: {
+                                    message: '请选择商品ID'
+                                }
+                            }
+                        }
+                    }
                 });
             }
         }
