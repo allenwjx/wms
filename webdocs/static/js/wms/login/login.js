2587eb0a0f2fc0e0233d8fbaa13e05964a063951
wjx20000 <wjx20000@ly.com>
2018-02-26 02:27:45 +0800

    登录页面

--- webdocs/static/js/wms/login/login.js
+++ webdocs/static/js/wms/login/login.js
@@ -5,58 +5,93 @@ $(document).ready(function () {
         data: {
             model: {}
         },
+        ready: function () {
+            this.handleLogin();
+        },
         methods: {
+            handleLogin: function () {
+                var self = this;
+                $('.login-form').validate({
+                    errorElement: 'span', //default input error message container
+                    errorClass: 'help-block', // default input error message class
+                    focusInvalid: false, // do not focus the last invalid input
+                    rules: {
+                        username: {
+                            required: true
+                        },
+                        password: {
+                            required: true
+                        },
+                        remember: {
+                            required: false
+                        }
+                    },
+
+                    messages: {
+                        username: {
+                            required: "Username is required."
+                        },
+                        password: {
+                            required: "Password is required."
+                        }
+                    },
+
+                    invalidHandler: function (event, validator) { //display error alert on form submit
+                        $('#err').html("请输入用户名和密码.");
+                        $('.alert-danger', $('.login-form')).show(500);
+                    },
+
+                    highlight: function (element) { // hightlight error inputs
+                        $(element).closest('.form-group').addClass('has-error'); // set error class to the control group
+                    },
+
+                    success: function (label) {
+                        label.closest('.form-group').removeClass('has-error');
+                        label.remove();
+                    },
+
+                    errorPlacement: function (error, element) {
+                        error.insertAfter(element.closest('.input-icon'));
+                        $('.alert-danger', $('.login-form')).hide(500);
+                    }
+                });
+
+                $('.login-form input').keypress(function (e) {
+                    if (e.which == 13) {
+                        if ($('.login-form').validate().form()) {
+                            self.doLogin();
+                        }
+                        return false;
+                    }
+                });
+            },
             login: function () {
-                $("#loginForm").data('bootstrapValidator', null);
-                this.validator();
+                if ($('.login-form').validate().form()) {
+                    this.doLogin();
+                }
+            },
+            doLogin: function() {
                 var me = this.model;
                 var url = "/wms/login";
                 var method = "POST";
-                $("#loginForm").data('bootstrapValidator').validate();
-                if ($("#loginForm").data('bootstrapValidator').isValid()) {
-                    $.ajax({
-                        type: method,
-                        url: url,
-                        data: JSON.stringify(me),
-                        dataType: "json",
-                        contentType: "application/json",
-                        //成功返回之后调用的函数
-                        success: function (data) {
-                            if (data.success) {
-                                window.location.replace("/wms/index");
-                            } else {
-                                $('#loginModal .modal-dialog').addClass('shake');
-                                $('.error').addClass('alert alert-danger').html(data.errorMessage);
-                                $('input[type="password"]').val('');
-                                setTimeout(function () {
-                                    $('#loginModal .modal-dialog').removeClass('shake');
-                                }, 1000);
-                            }
-                        },
-                        error: function (err) {
-                            toastr.error(err, {timeOut: 1500, positionClass: "toast-top-center"});
+                $.ajax({
+                    type: method,
+                    url: url,
+                    data: JSON.stringify(me),
+                    dataType: "json",
+                    contentType: "application/json",
+                    //成功返回之后调用的函数
+                    success: function (data) {
+                        if (data.success) {
+                            window.location.replace("/wms/index");
+                        } else {
+                            $('#err').html(data.errorMessage);
+                            $('.alert-danger', $('.login-form')).show(500);
+                            $('input[type="password"]').val('');
                         }
-                    });
-                }
-            },
-            validator: function () {
-                $('#loginForm').bootstrapValidator({
-                    message: 'This value is not valid',
-                    fields: {
-                        username: {
-                            validators: {
-                                notEmpty: {
-                                    message: '请输入账号'
-                                }
-                            }
-                        },
-                        password: {
-                            validators: {
-                                notEmpty: {
-                                    message: '请输入密码'
-                                }
-                            }
-                        }
+                    },
+                    error: function (err) {
+                        toastr.error(err, {timeOut: 1500, positionClass: "toast-top-center"});
                     }
                 });
             }
